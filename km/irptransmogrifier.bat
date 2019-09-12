b6497fdff7d9b36228b0e58670d1487aaa413e41
Bengt Martensson <bengtmartensson@users.sourceforge.net>
2019-09-12 19:15:19 +0000

    Fixes to IrpTransmogrifier wrappers.

--- km/irptransmogrifier.bat
+++ km/irptransmogrifier.bat
@@ -1,1 +1,28 @@
-"%JAVA%" -cp RemoteMaster.jar org.harctoolbox.irp.IrpTransmogrifier  %* 
+@echo off
+
+REM This wrapper allows you to run IrpTransmogrifier with the Remotemaster distribution.
+REM For more information and support, see
+REM http://www.hifi-remote.com/forums/viewtopic.php?t=101943 ,
+REM https://github.com/bengtmartensson/IrpTransmogrifier , and
+REM http://www.harctoolbox.org/IrpTransmogrifier.html
+
+REM This file is for use with Windows/DOS.
+
+REM The command line name to use to invoke java; change if desired.
+REM Must be Java 8 or higher.
+
+REM The command line name to use to invoke java.exe, change if desired.
+set JAVA=java
+
+REM Where the files are located, change if desired
+set APPLICATIONHOME=%~dp0
+
+REM Configfile to use
+set CONFIGFILE=%APPLICATIONHOME%\IrpProtocols.xml
+
+REM Normally no need to change after this line
+
+set MAINCLASS=org.harctoolbox.irp.IrpTransmogrifier
+set JAR=%APPLICATIONHOME%\RemoteMaster.jar
+
+"%JAVA%" -cp "%JAR%" "%MAINCLASS%" --configfile "%CONFIGFILE%" %*
---
+++ km/irptransmogrifier.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+# This wrapper allows you to run IrpTransmogrifier with the Remotemaster distribution.
+# For more information and support, see
+# http://www.hifi-remote.com/forums/viewtopic.php?t=101943 ,
+# https://github.com/bengtmartensson/IrpTransmogrifier , and
+# http://www.harctoolbox.org/IrpTransmogrifier.html
+
+# This file is for use with Linux and similar systems.
+
+# It is recommended to make a symbolic link to this file
+# from /usr/local/bin, i.e.
+#
+# ln -s <this-file> /usr/local/bin/irptransmogrifier
+
+# The command line name to use to invoke java; change if desired.
+# Must be Java 8 or higher.
+JAVA=java
+
+# Where the files are located; change if desired
+RMHOME="$(dirname -- "$(readlink -f -- "${0}")" )"
+
+# Configfile to use
+CONFIGFILE=${RMHOME}/IrpProtocols.xml
+
+# Normally no need to change anything after thins line
+
+JAR=${RMHOME}/RemoteMaster.jar
+MAINCLASS=org.harctoolbox.irp.IrpTransmogrifier
+
+exec "${JAVA}" -cp "${JAR}" "${MAINCLASS}" --configfile "${CONFIGFILE}" "$@"
